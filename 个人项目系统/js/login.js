22daed37bd085e9cfbac977b1b543cf66871bfeb
responsbility <87457187+responsbility@users.noreply.github.com>
2021-08-20 18:18:24 +0800

    Add files via upload

---
+++ 个人项目系统/js/login.js
@@ -0,0 +1,5 @@
+setTimeout(function(){alert("欢迎登录个人项目系统\n登录方法：直接点击登录\n第三方登录纯属娱乐，请勿扫码登录");},2000);
+setTimeout(function(){alert("声明：本系统无需输入用户名、密码或第三方登录，谨防诈骗");},2000);
+function login(){
+	window.top.location="main.html";
+}
